bde93b7966c588218cc3e77589e4200749c4f32b
Navdeep Patel <n-patel3@ti.com>
2026-01-26 01:23:42 +0530

    Hello world printed by basic bootloader

--- boot.asm
+++ boot.asm
@@ -1,12 +1,29 @@
-ORG 0x7c000
+ORG 0x7c00
 BITS 16
 
 start:
-    mov ah,0eh
-    mov al,'A'
+    mov si, msg
     mov bx,0
+
+print_loop:
+    lodsb
+    cmp al, 0
+    je done
+    call print_char
+    jmp print_loop
+
+print_char:
+    mov ah, 0x0E
     int 0x10
-    jmp $
+    ret
+
+done:
+    cli
+    hlt
+
+msg db 'Hello, World!',0
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+    
