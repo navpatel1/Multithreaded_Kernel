4926d1cb51b2f85320684b97049268dbc77ceacd
Navdeep Patel <n-patel3@ti.com>
2026-01-30 00:12:47 +0530

    Add new line and word wrapping system based on display size of 80x20

--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,71 @@
 #include "kernel.h"
 #include <stdint.h>
+#include <stddef.h>
 
-uint8_t *video_memory = (uint8_t *)0xb8000;
+uint16_t *video_memory = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_column = 0;
 
-void print_message(){
-    char message[] = "Hello! world";
-    int strlen = sizeof(message)/sizeof(message[0]);
-    for(int i = 0; i < strlen; i++){
-        video_memory[i * 2] = message[i];
-        video_memory[i * 2 + 1] = 0x06; // Light grey on black background
+//(x = row, y = column)
+
+uint16_t terminal_make_char(char c, uint8_t color)
+{
+    return (uint16_t)c | (uint16_t)color << 8;
+}
+
+void terminal_putchar(char c, uint8_t color, size_t x, size_t y)
+{
+    video_memory[x * VGA_WIDTH + y] = terminal_make_char(c, color);
+}
+
+void terminal_writechar(char c, uint8_t color)
+{
+    if(c == '\n') {
+      terminal_column = 0;
+      terminal_row++;
+      return;
+    }
+    terminal_putchar(c, color, terminal_row, terminal_column);
+    terminal_column++;
+    if(terminal_column >= VGA_WIDTH){
+        terminal_column = 0;
+        terminal_row++;
     }
 }
 
+void terminal_initialize()
+{
+    video_memory = (uint16_t*)0xB8000;
+    terminal_row = 0;
+    terminal_column = 0;
+    for(int x = 0; x < VGA_HEIGHT; x++){
+        for(int y = 0; y < VGA_WIDTH; y++){
+            terminal_putchar(' ', 0x0, x, y);
+        }
+    }
+}
+
+size_t strlen(const char* str){
+    size_t len = 0;
+    while(str[len] != '\0'){
+        len++;
+    }
+    return len;
+}
+
+void print(const char* str){
+    size_t len = strlen(str);
+    for(size_t i = 0; i < len; i++){
+        terminal_writechar(str[i], 15);
+    }
+}
+
+
+
 void kernel_main(){
-    print_message();
+
+    terminal_initialize();
+    print("Hello, Kernel World!\nAdded new line support.\nC17 standard in use.\n");
     while(1);  
 
 }
